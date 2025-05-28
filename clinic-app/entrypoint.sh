abf1abc56576698e416b34d21e2e9165ec36d585
docker0-dot <valera-yakusheva@inbox.ru>
2025-05-28 22:57:49 +0500

    Update entrypoint.sh

--- clinic-app/entrypoint.sh
+++ clinic-app/entrypoint.sh
@@ -2,7 +2,7 @@
 # entrypoint.sh
 
 # Устанавливаем переменную FLASK_APP для миграций
-export FLASK_APP=main.py
+export FLASK_APP=app.main
 
 export FLASK_RUN_HOST=0.0.0.0
 # Выполняем миграции
@@ -10,5 +10,5 @@ flask db upgrade
 flask run
 
 # Запускаем приложение
-exec python main.py
+exec python app/main.py
 
