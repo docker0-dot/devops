e1a532e559fec014393247d3d46d294db4ece08e
docker0-dot <valera-yakusheva@inbox.ru>
2025-06-09 17:57:20 +0500

    Update entrypoint.sh

--- clinic-app/app/entrypoint.sh
+++ clinic-app/app/entrypoint.sh
@@ -1,1 +1,18 @@
+#!/bin/sh
+# entrypoint.sh
 
+# Устанавливаем переменные окружения
+export FLASK_APP=run.py
+export FLASK_RUN_HOST=0.0.0.0
+
+# Если нет каталога migrations, инициализируем его
+if [ ! -d "migrations" ]; then
+    flask db init
+    flask db migrate -m "Initial migration"
+fi
+
+# Выполняем миграции
+flask db upgrade
+
+# Запускаем Flask
+exec flask run
